7c74f8c8251ffaad9d6cf83ad818cfe516f51121
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-11-13 14:16:35 -0500

    13-11-2021

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(`.gallery`);
+
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML(`beforeend`, cardsMarkup);
+galleryContainer.addEventListener(`click`, onImageClick);
+
+function createImageCardsMarkup(items) {
+    return items
+        .map(({ preview, original, description}) => {
+            return `
+            <div class="gallery__item">
+  <a class="gallery__link" href="#">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+        })
+        .join(``);  
+}
 console.log(galleryItems);
+function onImageClick(evt) {
+    const clickEl = evt.target;
+    console.log(clickEl.dataset.source);
+
+}
